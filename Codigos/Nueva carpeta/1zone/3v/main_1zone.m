c0624d8d58bf200cbca3a784b13a70251c7214a8
richiasdf <ricardo.lopez@ug.uchile.cl>
2021-10-18 17:00:32 -0300

    asd

--- Codigos/Nueva carpeta/1zone/3v/main_1zone.m
+++ Codigos/Nueva carpeta/1zone/3v/main_1zone.m
@@ -1,10 +1,26 @@
 %& Clearing the workspace
 clear all
 
+
+% Set to true, to resume training from a saved agent
+resumeTraining = true;
+
+%Range of temperature wanted
+low = 22; %temperatura minima
+high = 25; % temperatura máxima
+
+%Sample Time & Simulation Time
+Ts = 60; %1 min - HVAC System sample Time
+Ts2 = 600; %10 min - Neural network Sample time
+Tf = 21600; % 6 horas  - Simulation Time
+
+%device for critic & actor
+device = "cpu";
+
 %% Load and open the system in simulink
 mdl2 = 'hvac_1zone_v3';
-open_system(mdl2)
-
+%open_system(mdl2)
+warning('off','all')
 %% Define model parameters
 load('values1z.mat')
 
@@ -27,73 +43,12 @@ numActions = actInfo.Dimension(1);
 %de RL en el modelo, y las informaciones de observación y acción.
 env = rlSimulinkEnv(mdl2,'hvac_1zone_v3/RL Agent',...
     obsInfo,actInfo);
-
-
 %Función para reiniciar el sistema
 %Esta función es útil cuando se quiere usar distintas condiciones iniciales
 %en cada inicio de un episodio.
-ci = [36;36];
 env.ResetFcn = @(in) localResetFcn(in);
-
-%Range of temperature wanted
-min_temp = 22;
-max_temp = 25;
-
-%Sample Time & Simulation Time
-
-
-
-Ts = 60; %1 min
-Ts2 = 600; %10 min
-Tf = 21600; % 6 horas
-
-
 %Set random seed.
 rng(0)
-
-%Red neuronal Critico
-%CAMBIAR
-%fullyConnectedLayer(84,'Name','CriticStateFC1')
-%tanhLayer('Name','CriticTanh1')
-%14 6 6
-statePath = [
-    featureInputLayer(numObservations,'Normalization','none','Name','State')
-    fullyConnectedLayer(80,'Name','CriticStateFC1')
-    tanhLayer('Name','CriticTanh1')
-    fullyConnectedLayer(64,'Name','CriticStateFC2')];
-actionPath = [
-    featureInputLayer(numActions,'Normalization','none','Name','Action')
-    fullyConnectedLayer(64,'Name','CriticActionFC1')];
-commonPath = [
-    additionLayer(2,'Name','add')
-    tanhLayer('Name','CriticTanh')
-    fullyConnectedLayer(1,'Name','CriticOutput')];
-
-criticNetwork = layerGraph();
-criticNetwork = addLayers(criticNetwork,statePath);
-criticNetwork = addLayers(criticNetwork,actionPath);
-criticNetwork = addLayers(criticNetwork,commonPath);
-criticNetwork = connectLayers(criticNetwork,'CriticStateFC2','add/in1');
-criticNetwork = connectLayers(criticNetwork,'CriticActionFC1','add/in2');
-
-criticOpts = rlRepresentationOptions('LearnRate',1e-03,'GradientThreshold',1);
-critic = rlQValueRepresentation(criticNetwork,obsInfo,actInfo,'Observation',{'State'},'Action',{'Action'},criticOpts);
-
-%Red neuronal Actor
-actorNetwork = [
-    featureInputLayer(numObservations,'Normalization','none','Name','State')
-    fullyConnectedLayer(32, 'Name','actorFC2')
-    tanhLayer('Name','ActorCommonRelu2')
-    fullyConnectedLayer(numActions,'Name','Action')
-    ];
-
-actorOptions = rlRepresentationOptions('LearnRate',1e-03,'GradientThreshold',1);
-
-
-%Ver mas en detalle
-actor = rlDeterministicActorRepresentation(actorNetwork,obsInfo,actInfo,'Observation',{'State'},'Action',{'Action'},actorOptions);
-
-
 %Opciones del agente
 agentOpts = rlDDPGAgentOptions(...
     'SampleTime',Ts2,...
@@ -108,14 +63,11 @@ agentOpts = rlDDPGAgentOptions(...
 %utilizar un vector en vez de un escalar.
 agentOpts.NoiseOptions.StandardDeviation = [3.35; 3.4; 3.2];
 agentOpts.NoiseOptions.StandardDeviationDecayRate = [1e-4; 5e-4 ; 1e-3];
-% Averiguar***
 agentOpts.NoiseOptions.MeanAttractionConstant = 0.15/Ts2;
 agentOpts.NoiseOptions.StandardDeviationMin = [0.015; 0.01; 0.01];
 
 
 % Train the agent
-% Set to true, to resume training from a saved agent
-resumeTraining = true;
 % Set ResetExperienceBufferBeforeTraining to false to keep experience from the previous session
 agentOpts.ResetExperienceBufferBeforeTraining = ~(resumeTraining);
 
@@ -123,26 +75,29 @@ if resumeTraining
     agentOpts.NoiseOptions.StandardDeviation = [2.25; 1.2; 1.15];
     load('train_agent.mat');
 else
+    %Red neuronal Critico
+    criticOpts = rlRepresentationOptions('LearnRate',1e-03,'GradientThreshold',1, 'UseDevice', device);
+    criticNetwork = MakeCritic(numObservations,numActions,[2,1],[80, 64, 64]);
+    critic = rlQValueRepresentation(criticNetwork,obsInfo,actInfo,'Observation',{'State'},'Action',{'Action'},criticOpts);
+    %Red neuronal Actor
+    actorNetwork = MakeActor(numObservations,numActions, 1 , 80);
+    actorOptions = rlRepresentationOptions('LearnRate',1e-03,'GradientThreshold',1, 'UseDevice', device);
+    actor = rlDeterministicActorRepresentation(actorNetwork,obsInfo,actInfo,'Observation',{'State'},'Action',{'Action'},actorOptions);
     % Create a fresh new agent
     agent = rlDDPGAgent(actor, critic, agentOpts);
 end
-
-
-
-maxepisodes = 25000;
+maxepisodes = 55000;
 maxsteps = ceil(Tf/Ts2);
 StopReward = -7;
-%    'Verbose',false, ...
+%    'Verbose',true, ...      'Plots','none',...
 trainOpts = rlTrainingOptions(...
     'MaxEpisodes',maxepisodes, ...
     'MaxStepsPerEpisode',maxsteps, ...
     'UseParallel',true, ...
     'ScoreAveragingWindowLength',25, ...
-    'Plots','training-progress',...
+    'Verbose',true, ...
     'StopTrainingCriteria','AverageReward',...
     'StopTrainingValue',StopReward);
-
-
 %Se comienza a entrenar el agente y se abre el gráfico de entrenamiento.
 trainingStats = train(agent,env,trainOpts);
 
@@ -154,5 +109,45 @@ simOpts = rlSimulationOptions('MaxSteps',maxsteps,'StopOnError','on');
 sim(env,agent,simOpts);
 
 %Save the train network
-save('train_agent.mat','agent','simOpts')
+%save('train_agent.mat','agent','simOpts')
 
+
+%% Funciones a utilizar
+
+function criticNetwork = MakeCritic(obs,acts,layers,neurons)
+    statePath = featureInputLayer(obs,'Normalization','none','Name','State');
+    for i=1:layers(1)
+        if even(i)
+            statePath = [statePath; tanhLayer('Name','StateTanh'+string(i/2))];
+        end
+        statePath = [statePath; fullyConnectedLayer(neurons(i),'Name','CriticStateFC'+string(i))]; 
+        
+    end
+    actionPath = featureInputLayer(acts,'Normalization','none','Name','Action');
+    for i=1:layers(2)
+        if even(i)
+            actionPath = [actionPath;tanhLayer('Name','ActionTanh'+string(i/2))];
+        end
+        actionPath = [actionPath; fullyConnectedLayer(neurons(i+layers(1)),'Name','CriticActionFC'+string(i))]; 
+    end
+    commonPath = [
+        additionLayer(2,'Name','add')
+        tanhLayer('Name','CriticTanh')
+        fullyConnectedLayer(1,'Name','CriticOutput')];
+
+    criticNetwork = layerGraph();
+    criticNetwork = addLayers(criticNetwork,statePath);
+    criticNetwork = addLayers(criticNetwork,actionPath);
+    criticNetwork = addLayers(criticNetwork,commonPath);
+    criticNetwork = connectLayers(criticNetwork,'CriticStateFC'+string(layers(1)),'add/in1');
+    criticNetwork = connectLayers(criticNetwork,'CriticActionFC'+string(layers(2)),'add/in2');
+    
+end
+
+function Network = MakeActor(obs,acts,layers,neurons)
+    Network = featureInputLayer(obs,'Normalization','none','Name','State');
+    for i=1:layers(1)
+        Network = [Network; fullyConnectedLayer(neurons(i),'Name','ActorFC'+string(i)); tanhLayer('Name','ActorTanh'+string(i/2)) ]; 
+    end
+    Network = [Network; fullyConnectedLayer(acts,'Name','Action')];
+end
